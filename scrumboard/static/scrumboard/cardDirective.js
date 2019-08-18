75ed632f010738ab5daa862b9544b87b1ca3afd3
prathmesh36 <prathmesh36@yahoo.com>
2019-08-18 20:22:05 +0530

    First Commit

---
+++ scrumboard/static/scrumboard/cardDirective.js
@@ -0,0 +1,49 @@
+(function(){
+    'use strict';
+    angular.module('scrumboard.demo')
+        .directive('scrumboardCard',CardDirective);
+
+    function CardDirective(){
+        return {
+            templateUrl:'/static/scrumboard/card.html',
+            restrict: 'E',
+            controller: ['$scope','$http', function($scope,$http){
+                var url = '/scrumboard/cards/'+$scope.card.id+'/';
+                $scope.update = function(){
+                    $http.put(url,$scope.card).then(function(){
+                        return false;
+                    });
+                };
+                
+                $scope.delete =  function(){
+                    //alert($scope.list.id);
+                    $http.delete(url).then(
+                    function(){
+                        var cards = $scope.list.cards;
+                        cards.splice(cards.indexOf($scope.card),1);
+                        return false;
+                    }
+                    );
+                };
+
+                $scope.movecard = function(movelist,card){
+                    card.list=movelist;
+                    $http.put(url,card).then(function(){
+                        var cards = $scope.list.cards;
+                        cards.splice(cards.indexOf($scope.card),1);
+                        for(var key in $scope.data){
+                            var list = $scope.data[key];
+                            if(list.id==movelist){
+                                list.cards.push(card);
+                            }
+                        }
+                        $('.modal-backdrop').hide(); 
+                        //alert("Move Success");
+                    });
+                };
+
+
+            }]
+        };
+    }
+})();
---
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -0,0 +1,55 @@
+(function(){
+    'use strict';
+    angular.module('scrumboard.demo',[])
+    .controller('ScrumboardController', ['$scope','$http', ScrumboardController]);
+
+    function ScrumboardController($scope,$http){
+        $scope.data =[];
+        $http.get('/scrumboard/lists/').then(
+            function(response){
+                $scope.data=response.data;
+            }
+        );
+
+        $scope.add = function(list,title){
+            var card ={
+                list:list.id,
+                title:title
+            };
+            $http.post('/scrumboard/cards/',card)
+                .then(function(response){
+                    list.cards.push(response.data);
+                },
+                function(){
+                    alert('Failed to create a card');
+                }
+                );
+        };
+
+        $scope.addlist = function(data,name){
+            var list={
+                name:name,
+                project:1
+            };
+            $http.post('/scrumboard/lists/',list)
+                .then(function(response){
+                    data.push(response.data);
+                },
+                function(){
+                    alert('Failed to create a List');
+                }
+                );
+        };
+
+        $scope.deletelist =  function(list,data){
+            //alert(list.id);
+            var url = '/scrumboard/lists/'+list.id+'/';
+            $http.delete(url).then(
+                function(){
+                    data.splice(data.indexOf(list),1);
+                    $('.modal-backdrop').hide(); 
+                }
+            );
+        };
+    }
+}());
